49be5d736bed9a9c15f91324bb37445b941b404c
Jeff_Gsa <jeromegbossa7@gmail.com>
2024-08-22 00:05:34 +0200

    mise à jour

--- GesEmploye/App.java
+++ GesEmploye/App.java
@@ -3,30 +3,18 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        Employe e = new Employe(4, "skj", "hhjh", 63666);
-        Scanner scanner=new Scanner(System.in);
-        ArrayList<Employe> bdd= new ArrayList<Employe>(50);
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("*****GESTION DES EMPLOYES *****");
+    System.out.println("*****Que voulez vous faire ? *****");
+    int choix=scanner.nextInt();
+    switch(choix){
+        case 1:Employe.registerEmploye();
 
-        System.out.println("Veuillez entrer votre nom");
-        String n=scanner.nextLine();
-        System.out.println("Veuillez entrer votre poste");
-        String poste=scanner.nextLine();
-        System.out.println("Veuillez entrer votre salaire");
-        int s=scanner.nextInt();
+            break;
+        case 2: ;
 
-        e.setID(5);
-        e.setNom(n);
-        e.setPoste(poste);
-        e.setSalaire(s);
-        bdd.add(e);
-        System.out.println(bdd);
-       
-            for(Employe i:bdd){
-                System.out.println("Voici la liste des employe");
-                System.out.println(i.getNom());
-                System.out.println(i.getPoste());
-                System.out.println(i.getSalaire());
-            }
+    }
+
         }
     }
 
--- GesEmploye/Employe.java
+++ GesEmploye/Employe.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Employe {
+
     
          int ID=0;
         int salaire=0;
@@ -44,11 +47,44 @@ public class Employe {
             this.poste = poste;
         }
     
-        // public void afficherEmploye( e){
-        //     for(Employe i:e){
-        //         System.out.println(i.getSalaire());
-        //     }
-        // }
+        public static void afficherEmploye(ArrayList<Employe> e){
+            for(Employe i:e){
+                System.out.println("Voici la liste des employe");
+                System.out.println(i.getNom());
+                System.out.println(i.getPoste());
+                System.out.println(i.getSalaire());            }
+        }
+
+
+
+
+
+     public static void registerEmploye(){
+        System.out.println("Combien d'employé voulez-vous enregistrer?");
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        ArrayList<Employe> bdd= new ArrayList<Employe>(50);
+        for (int i=0;i<n;i++)
+        {
+
+            Employe e = new Employe(4, "skj", "hhjh", 63666);
+            System.out.println("Veuillez entrer votre nom");
+            String nom=scanner.nextLine();
+            System.out.println("Veuillez entrer votre poste");
+            String poste=scanner.nextLine();
+            System.out.println("Veuillez entrer votre salaire");
+            int s=scanner.nextInt();
+
+            e.setID(5);
+            e.setNom(nom);
+            e.setPoste(poste);
+            e.setSalaire(s);
+            bdd.add(e);
+        } 
+
+        }
+
+
         
     }
     
